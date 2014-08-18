168fa5b7347a02e7421527134e72e7122065e704
Petr Bouchal <pbouchal@gmail.com>
2014-08-18 19:22:09 +0100

    Organise files

---
+++ URLs for GovUK google analytics.R
@@ -0,0 +1,25 @@
+# Top content by department
+'https://www.performance.service.gov.uk/data/gov-uk-content/top-count?filter_by=department%3Adepartment-for-business-innovation-skills&group_by=pageTitle&collect=pageviews%3Asum&collect=uniquePageviews%3Asum&period=week&duration=10'
+'top-news-count'
+'top-publications-count'
+'top-consultations-count'
+'top-policy-count'
+'top-speeches-count'
+'top-collections-count'
+
+# Analogous but without departmental cut:
+'https://www.performance.service.gov.uk/data/gov-uk-content/top-count?group_by=pageTitle&collect=pageviews%3Asum&collect=uniquePageviews%3Asum&period=week&duration=10'
+
+# Traffic - by department, for all departments
+'https://www.performance.service.gov.uk/data/gov-uk-content/traffic-count?collect=visitors%3Asum&group_by=department&period=week&start_at=2014-04-28T00%3A00%3A00%2B00%3A00&end_at=2014-06-30T00%3A00%3A00%2B00%3A00'
+
+# Top content, overall:
+'https://www.performance.service.gov.uk/data/govuk/most_viewed?sort_by=pageviews%3Adescending&limit=10'
+'https://www.performance.service.gov.uk/data/govuk/most_viewed_policies?sort_by=pageviews%3Adescending&limit=10'
+'https://www.performance.service.gov.uk/data/govuk/most_viewed_news?sort_by=pageviews%3Adescending&limit=10'
+
+# Most commented, by department:
+'https://www.performance.service.gov.uk/data/gov-uk-content/top-urls?filter_by=organisation_acronym%3Aco&group_by=url&collect=comment_count%3Asum&period=month&start_at=2014-06-01T00%3A00%3A00%2B00%3A00&end_at=2014-07-01T00%3A00%3A00%2B00%3A00'
+
+# Most commented, all (not sure this returns what it should)
+'https://www.performance.service.gov.uk/data/gov-uk-content/top-urls?group_by=url&collect=comment_count%3Asum&period=month&duration=10&limit=10'
